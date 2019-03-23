7cac3d48de19d2b045717e522d6abcfdd13f85b7
Bryan <bsteven1993@gmail.com>
2019-03-22 20:51:33 -0500

    ajuste defaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -45,10 +45,6 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        // replace this example code with whatever you need
-        /* return $this->render('default/index.html.twig', [
-             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-         ]);*/
         $em = $this->getDoctrine()->getManager();
         $productos = $em->getRepository(Producto::class)
             ->findAll();
